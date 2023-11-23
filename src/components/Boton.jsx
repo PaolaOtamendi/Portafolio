61f1ece8dc68115ccfb7468f9a01eedc984fa794
PaolaOtamendi <paola_otamendi23@hotmail.com>
2023-11-23 16:13:14 -0600

    feat componente aboutme, botonlets

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,7 +1,26 @@
-const Boton = () => {
-    return(
-        <></>
-    )
-}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
-export default Boton;
+ export const BotonLets = () => {
+  return (
+    <div>
+      <Link to="/Me">
+        <button
+          className="transition ease-in-out delay-150 bg-white-500 hover:-translate-y-1 hover:scale-110 hover:bg-purple-500 hover:text-white duration-300
+          bg-white text-black text-lg rounded-lg p-2 font-bold m-3 flex items-center cursor-pointer"
+        >
+          Let´s Go!
+          <FontAwesomeIcon
+            icon={faStar}
+            spin
+            size="lg"
+            style={{ color: "#00ff91" }}
+            className="ml-2"
+          />
+        </button>
+      </Link>
+    </div>
+  );
+};
+
