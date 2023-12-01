51aa1f85d2db4045fc43f5e78210852bd25d3b4b
PaolaOtamendi <paola_otamendi23@hotmail.com>
2023-12-01 14:19:07 -0600

    feat componente proyecto, botonproyecto, fix cambios en componete proyectos

---
+++ src/components/Proyecto.jsx
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
+import { BotonProject } from "./Boton.jsx";
+
+
+export const Proyecto = ({ titulo, descripcion, herramientas, imagen, githubLink, demoLink, }) => {
+  return (
+    <div className="proyecto">
+      <h1 className="titleApp">{titulo}</h1>
+      <div className="dataproyecto">
+        <div className="containerdata">
+          <p className="description">{descripcion}</p>
+          <div className="herramientas">
+            {herramientas.map((herramienta, index) => (
+              <p key={index} className="tool">
+                <FontAwesomeIcon
+                  icon={faSquareCheck}
+                  style={{ color: "#ac1ee0" }}
+                  className="mr-3"
+                />
+                {herramienta}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="containerimage">
+          <img src={imagen} alt={titulo} className="image" />
+        </div>
+      </div>
+      <div className="containbutton">
+        <BotonProject githubLink={githubLink} demoLink={demoLink} />
+      </div>
+    </div>
+  );
+};
